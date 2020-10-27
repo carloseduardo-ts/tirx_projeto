9e526eec54a674f92f1136cfc4a3760209e443ec
c4loseduard0 <c4rloseduard0@alu.ufc.br>
2020-10-27 14:42:31 -0300

    formatando dados para envio ia post

--- app.py
+++ app.py
@@ -1,6 +1,39 @@
 import xlrd
 import requests
 
+base_url = "http://demo.trixlog.com/trix/"
+
+data = '''
+{
+    "id": {id},
+    "name": {name},
+    "type": {type},
+    "registration": {registration},
+    "vehicles": [],
+    "driverTeam": {
+        "id": {driverTeam_id}
+    },
+    "rg": {rg},
+    "cpf": {cpf},
+    "status": {status},
+    "hiringType": {hiring_type},
+    "riskDriver": {risk_driver},
+    "integrationId": {integration_id},
+    "licenseCategory": {l_category},
+    "licenseExpedition": {l_expedition},
+    "licenseExpiration": {l_expiration},
+    "licenseRegister": {l_register},
+    "registrationCode": {registration_code}
+}
+'''
+
 wb = xlrd.open_workbook('teste.xlsx')
 ws = wb.sheet_by_index(0)
+
+
+
+
+
+
+
 
